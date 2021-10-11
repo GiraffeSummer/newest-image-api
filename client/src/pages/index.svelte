a38bcf74ad9a8308fc07dea886910e907586af7f
GiraffeSummer <39410674+GiraffeSummer@users.noreply.github.com>
2021-10-11 15:55:55 +0200

    updated index text

--- client/src/pages/index.svelte
+++ client/src/pages/index.svelte
@@ -1,7 +1,7 @@
 <script>
   import { metatags } from '@roxi/routify';
-  import {backend} from '../stores.js'
   import Navbar from '../components/navbar.svelte';
+  import { backend } from '../stores.js';
 
   metatags.title = 'Gif Monkey';
   metatags.description = 'Api for (reaction) gifs';
@@ -12,4 +12,9 @@
 <p>
   Hello this is work in progress, but please try to log in upload something,
   then see if you can find it on the /find page
+</p>
+<p>
+  I made this mainly for myself to use for my discord bot but feel free to
+  message me to use it too for your bot or something else (if you know this
+  website you most likely know me XD)
 </p>
