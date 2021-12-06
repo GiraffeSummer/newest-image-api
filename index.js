42f3dc369e278a4648233f67db20204e7d7bb3e3
GiraffeSummer <39410674+GiraffeSummer@users.noreply.github.com>
2021-12-06 12:47:30 +0100

    test 7

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.use(
         contentSecurityPolicy: {
             directives: {
                 ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-                'img-src': ['\'self\'', 'http://*', 'https://*', 'https://cdn.discordapp.com'],
+                'img-src': ["'self'", 'https://cdn.discordapp.com/'],
             },
         },
     })
