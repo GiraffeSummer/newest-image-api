2eafbc306deceb3b118368a72aa58f590bd5fa6f
GiraffeSummer <39410674+GiraffeSummer@users.noreply.github.com>
2021-12-06 13:59:12 +0100

    test 1

--- client/src/pages/user/uploads.svelte
+++ client/src/pages/user/uploads.svelte
@@ -15,7 +15,7 @@
   metatags.description = 'User uploads';
 
   onMount(async () => {
-    const res = await fetch(backend + '/user/file-uploads' + (showNsfw) ?'?nsfw=true' :'', {
+    const res = await fetch(backend + '/user/file-uploads' + (showNsfw) ? '?nsfw=true' : '' , {
       credentials: 'include',
     });
     let data = await res.json();
@@ -31,7 +31,7 @@
 
   let request = undefined;
   const GetUserUploads = async () => {
-    const res = await fetch(backend + '/user/uploads/' + selected  + (showNsfw) ?'?nsfw=true' :'', {
+    const res = await fetch(backend + '/user/uploads/' + selected  + (showNsfw) ? '?nsfw=true' : '', {
       credentials: 'include',
     });
     const data = await res.json();
@@ -68,10 +68,8 @@
   {:else}
     <h3>{userData.user.username}'s uploads</h3>
   {/if}
-  <label for="showNsfw" value="Show Nsfw?"/>
-  <input name="showNsfw" type=checkbox bind:checked={showNsfw} on:change={() => {
-        if (userData.user._id != selected) request = GetUserUploads();
-      }}/>
+  <label for="showNsfw" value="Show Nsfw?" />
+  <input name="showNsfw" type=checkbox bind:checked={showNsfw}  />
   {#if userData.user._id == selected}
     {#each userData.uploads as gif}
       <Upload {gif} on:deletegif={deleteGif} />
