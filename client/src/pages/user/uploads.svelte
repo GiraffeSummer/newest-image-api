5a339dc74358c4fcec67b57b46beda4965fbe631
GiraffeSummer <39410674+GiraffeSummer@users.noreply.github.com>
2021-12-09 14:33:59 +0100

    testing...

--- client/src/pages/user/uploads.svelte
+++ client/src/pages/user/uploads.svelte
@@ -42,6 +42,7 @@
       credentials: 'include',
     });
     const data = await res.json();
+    console.log(userData,'selected: '+ selected)
     canDelete = (userData.user._id == selected) ? true : userData.user.permissions.includes('delete_content');
     return data;
   };
