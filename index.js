3a8bdcf79d3213fb6b80ff31197cc89593636af0
GiraffeSummer <gidoselten@hotmail.com>
2021-12-13 19:29:49 +0100

    temporary fix

--- index.js
+++ index.js
@@ -131,20 +131,10 @@ app.use(cors({
             return callback(new Error(msg), false);
         } return callback(null, true);
     },*/
-    //origin: '*',//allowedOrigins,
-    origin: true,
+    origin: allowedOrigins,
     credentials: true
 }));
 
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials","true");
-    res.header("Access-Control-Allow-Methods", "GET, DELETE, PUT, POST"); 
-    next();
-  }); 
-  
 
 app.use(
     helmet({
