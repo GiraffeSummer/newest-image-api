801daec2b610b2f1a1ffa8af4a9408efaed866b8
GiraffeSummer <gidoselten@hotmail.com>
2022-01-28 13:32:49 +0100

    hot module reload

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,14 @@
 import './global.css';
-import App from './App.svelte';
+/*import App from './App.svelte';
 
 const app = new App({
     target: document.body,
-});
+});*/
+
+import HMR from "@roxi/routify/hmr";
+import App from "./App.svelte";
+
+const app = HMR(App, { target: document.body }, "routify-app");
+
 
 export default app;
