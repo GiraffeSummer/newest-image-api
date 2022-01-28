6de840eeb5268b87f8e27c21426c22884f38ebf2
GiraffeSummer <gidoselten@hotmail.com>
2022-01-28 16:37:38 +0100

    scrolling code block

--- client/src/pages/find/_results.svelte
+++ client/src/pages/find/_results.svelte
@@ -2,14 +2,21 @@
   import Pager from '../../components/Pager.svelte';
   import { gifImages, lastSearch, result } from './find.js';
   import SimpleGif from './SimpleGif.svelte';
-  export let page
+  export let page;
 </script>
 
 {#if $gifImages.length > 0}
-  <Pager list={$gifImages} component={SimpleGif} prop='gif' {page}/>
+  <Pager list={$gifImages} component={SimpleGif} prop="gif" {page} />
   <br />
   <h3>Raw:</h3>
   <div id="code"><pre>{$result}</pre></div>
 {:else if $lastSearch != ''}
   <h6><i>Nothing found...</i></h6>
 {/if}
+
+<style>
+  pre {
+    overflow: scroll;
+    height: 500px;
+  }
+</style>
