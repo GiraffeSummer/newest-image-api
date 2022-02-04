33a792c7ce75032779543e5067419ff5969522b2
GiraffeSummer <gidoselten@hotmail.com>
2022-02-04 11:46:21 +0100

    fixed ref to analytics

--- client/src/pages/_layout.svelte
+++ client/src/pages/_layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import '../../node_modules/mini.css/dist/mini-dark.min.css';
-  import Analytics from '../../../components/Analytics.svelte';
+  import Analytics from '../../components/Analytics.svelte';
   import Navbar from '../components/navbar.svelte';
 </script>
 
