f3311a4e7c35d3038aa5071b6f7c5b7a5034aef1
GiraffeSummer <39410674+GiraffeSummer@users.noreply.github.com>
2022-02-04 11:37:39 +0100

    moved tracking to app.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -15,6 +15,19 @@
 
   metatags.description = "gifmonkey gif monkey - (anime) reaction gif API and platform";
   metatags.robots = "index, follow";
+
+
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+      let u = "//matomo.cripplerick.com/";
+      _paq.push(['setTrackerUrl', u + 'matomo.php']);
+      _paq.push(['setSiteId', '2']);
+      let d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+      g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    })();
 </script>
 
 <Router {routes} />
