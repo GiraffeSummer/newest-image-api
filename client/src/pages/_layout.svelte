461054c0f1801667fed6b2cfb1f53b79bc520a2d
GiraffeSummer <gidoselten@hotmail.com>
2022-02-04 11:45:52 +0100

    fixed analitics?

--- client/src/pages/_layout.svelte
+++ client/src/pages/_layout.svelte
@@ -1,7 +1,11 @@
 <script>
-  import "../../node_modules/mini.css/dist/mini-dark.min.css"
+  import '../../node_modules/mini.css/dist/mini-dark.min.css';
+  import Analytics from '../../../components/Analytics.svelte';
   import Navbar from '../components/navbar.svelte';
 </script>
 
+<svelte:head>
+  <Analytics />
+</svelte:head>
 <Navbar />
 <slot />
