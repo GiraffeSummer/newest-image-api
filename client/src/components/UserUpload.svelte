0d56adc8e8e7cea02d974463c0a1038fbf4f2fed
GiraffeSummer <gidoselten@hotmail.com>
2022-02-08 16:02:38 +0100

    undid props

--- client/src/components/UserUpload.svelte
+++ client/src/components/UserUpload.svelte
@@ -16,7 +16,6 @@
 
   export let gif;
   export let properties = {};
-  export let tags, filename, isnsfw;
   let canDelete = properties.canDelete || false;
 
   const updateGif = async () => {
@@ -40,11 +39,7 @@
     if (res.status == 'ok') createMessage(`Gif was updated`);
   };
 
-  $: {
-    tags = gif.tags;
-    filename = gif.name;
-    isnsfw = gif.nsfw;
-  }
+  let { tags, name: filename, nsfw: isnsfw } = gif;
 
   tags = tags.join(', ');
 
